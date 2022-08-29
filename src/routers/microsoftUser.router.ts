e8393065df529d3733bea49a6bbe6f5600103f66
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-08-29 23:55:50 +0530

    Implement-microsoftUser.router

--- src/routers/microsoftUser.router.ts
+++ src/routers/microsoftUser.router.ts
@@ -2,6 +2,11 @@ import express, { Request, Response } from "express";
 import microsoftUser from "../schema/microsoftUserSchema";
 import leaderShip from "../schema/leaderShipSchema";
 import { Rollbar } from "../helpers/Rollbar";
+import ErrorResponse from "../models/ErrorResponse.model";
+import {
+  graphReportingsValidation,
+  exceptionalValidation,
+} from "../services/microsoftUserService";
 
 const router = express.Router();
 
@@ -18,10 +23,72 @@ router.get("/", (req: Request, res: Response) => {
     getUserById(userId.toString()).then((result) => {
       res.status(200).json({ data: result });
     });
-
   } catch (error) {
     Rollbar.error(error as unknown as Error, req);
     res.status(500).json({ message: (error as unknown as Error).message });
+  }
+});
+
+router.get("/clone-reportings", async (req: Request, res: Response) => {
+  try {
+    const authorisedCloneReportingUsers: string[] = [
+      "d12d68ea-b04f-4bd8-9124-becae7acb9b5",
+      "0d5fd023-dbba-4996-8752-52aa716e83a3",
+      "4853e42e-0927-4daa-987a-a361c83fb6be",
+      "1ffd7271-6822-43e2-a3a8-2bac5839f7a7",
+      "781d5e17-5dff-48da-a84f-c9420c0ed957",
+    ];
+
+    const { fromUserId, toUserId, userId } = req.query;
+
+    if (!userId) {
+      res.status(400).json({ message: "UserId doesn't exist" });
+    }
+    if (!fromUserId) {
+      res.status(400).json({ message: "fromUserId doesn't exist" });
+    }
+    if (!toUserId) {
+      res.status(400).json({ message: "touserId doesn't exist" });
+    }
+    if (userId) {
+      if (!authorisedCloneReportingUsers.includes(userId.toString())) {
+        res.status(401).json({ message: "Unauthorised userId" });
+      }
+    }
+
+    var fromUserIdQuery = {
+      userId: fromUserId,
+    };
+    var toUserIdQuery = {
+      userId: toUserId,
+    };
+
+    var fromUserIdDResult: any = await microsoftUser.findOne(fromUserIdQuery);
+    var toUserIdResult: any = await microsoftUser.findOne(toUserIdQuery);
+
+    if (fromUserIdDResult.reportings.length <= 1) {
+      res.status(400).json({
+        message: "Reportings was not exists for the given fromUserId",
+      });
+    }
+
+    var mergeResults = await fromUserIdDResult.reportings.concat(
+      toUserIdResult.reportings
+    );
+    const filteredReportings = mergeResults.filter(
+      (reportings: string, index: number) =>
+        mergeResults.indexOf(reportings) === index
+    );
+    const finalReportings = filteredReportings.filter(
+      (id: string) => id !== fromUserId
+    );
+
+    const response = await microsoftUser.findByIdAndUpdate(toUserIdResult._id, {
+      reportings: finalReportings,
+    });
+    return res.status(200).json({ response: response });
+  } catch (error) {
+    res.status(500).json({ message: error });
   }
 });
 
@@ -36,4 +103,5 @@ async function getUserById(id: String) {
     return { message: error.message };
   }
 }
+
 export { router as microsoftUserRouter };
