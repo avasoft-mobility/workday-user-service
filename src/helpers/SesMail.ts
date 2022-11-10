490242043709a4fe9fe42a80a0865ff95e557b77
Clement   Ambrose <Clement.A@avasoft.com>
2022-11-10 17:42:11 +0530

    Add access key and secret key

--- src/helpers/SesMail.ts
+++ src/helpers/SesMail.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const SES_CONFIG = {
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.SES_ACCESS_KEY_ID!,
+  secretAccessKey: process.env.SES_SECRET_ACCESS_KEY!,
+  region: process.env.AWS_REGION!,
 };
 
 const AWS = require("aws-sdk");
